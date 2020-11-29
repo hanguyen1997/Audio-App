7296f8b1c5d2733dd6147227ca2bd1fa2a2a9635
hanguyen1997 <truongbahanguyen@gmail.com>
2020-11-29 21:53:11 +0700

    add button on/off and control bar sound

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-
+/*-------*/
 var img = document.getElementById("img-body");
 var audio = document.getElementById("audio-body");
+var volume = document.getElementById("volume");
+
 
 $('document').ready(function(){
 
-	
-	load_mp3();
-		
 	$('#city').click(function(){
 		img.src = "image/city.gif";
 		audio.src  = "audio/city.mp3";
 		load_mp3();
-
-
 	});
 	$('#rain').click(function(){
 		img.src = "image/rain.gif";
@@ -30,7 +27,30 @@ $('document').ready(function(){
 		audio.src  = "audio/forest.mp3";
 		load_mp3();
 	});
+
+	/*điều chỉnh thanh chỉnh âm lượng*/
+	volume.oninput = function() {
+		var volume_number = (this.value)/100;
+		audio.volume  =  volume_number;
+	}
+
 })
+
+
+function change_volume(){
+	/*kiểm tra chuổi kí tự cuối cùng của src image bằng hàm endsWith*/
+	if(document.getElementById("change-volume").src.endsWith('image/mute.png'))
+	{
+		audio.volume  = 0;
+		document.getElementById("volume").value = 0;
+		document.getElementById("change-volume").src = "image/volume.png";
+	}
+	else{
+		audio.volume  = 0.5;
+		document.getElementById("volume").value = 50;
+		document.getElementById('change-volume').src='image/mute.png'
+	}
+}
 
 function load_mp3(){
 	audio.loop = true;
