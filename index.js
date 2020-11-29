67b9e2155019add203fff9a8bc81045fcff48557
hanguyen1997 <truongbahanguyen@gmail.com>
2020-11-30 00:23:09 +0700

    fix css, responsive < 800px

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*-------*/
-var img = document.getElementById("img-body");
+var img = document.getElementById("app");
 var audio = document.getElementById("audio-body");
 var volume = document.getElementById("volume");
 
@@ -7,23 +7,23 @@ var volume = document.getElementById("volume");
 $('document').ready(function(){
 
 	$('#city').click(function(){
-		img.src = "image/city.gif";
+		img.style.backgroundImage = "url('image/city.gif')";
 		audio.src  = "audio/city.mp3";
 		load_mp3();
 	});
 	$('#rain').click(function(){
-		img.src = "image/rain.gif";
+		img.style.backgroundImage = "url('image/rain.gif')";
 		audio.src  = "audio/rain.mp3";
 		load_mp3();
 	});
 	$('#beach').click(function(){
-		img.src = "image/beach.gif";
+		img.style.backgroundImage = "url('image/beach.gif')";
 		audio.src  = "audio/beach.mp3";
 		load_mp3();
 		
 	});
 	$('#forest').click(function(){
-		img.src = "image/forest.gif";
+		img.style.backgroundImage = "url('image/forest.gif')";
 		audio.src  = "audio/forest.mp3";
 		load_mp3();
 	});
@@ -32,6 +32,10 @@ $('document').ready(function(){
 	volume.oninput = function() {
 		var volume_number = (this.value)/100;
 		audio.volume  =  volume_number;
+
+		/*if volume = 0 image  volume.png and volume > 0 image = mute.png*/
+		if(volume_number > 0 ) document.getElementById("change-volume").src = "image/mute.png";
+		else document.getElementById("change-volume").src = "image/volume.png";
 	}
 
 })
